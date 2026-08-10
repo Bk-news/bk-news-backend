6c4bb797d72994ecc3f29a6c1d3898c927fc80fc
Bk-news <cjrzf824zy@privaterelay.appleid.com>
2026-08-10 15:01:19 +0300

    Create sources.js for news sources and keywords
    Added a module exporting news sources and keywords related to Saudi Arabia.

---
+++ sources.js
@@ -0,0 +1,22 @@
+module.exports = [
+  { name: "Al Arabiya (Saudi Arabia)", local: true,
+    url: "https://english.alarabiya.net/feed/rss2/en/News.xml" },
+  { name: "Okaz", local: true,
+    url: "https://www.okaz.com.sa/rssFeed/0" },
+  { name: "Al Yaum", local: true,
+    url: "https://www.alyaum.com/rssFeed/1" },
+  { name: "Al Eqtisadiah", local: true,
+    url: "https://www.aleqt.com/rss" },
+
+  { name: "Al Jazeera English", local: false,
+    url: "https://www.aljazeera.com/xml/rss/all.xml" },
+  { name: "Al Arabiya (Top Stories, Intl)", local: false,
+    url: "https://english.alarabiya.net/feed/rss2/en.xml" },
+];
+
+module.exports.keywords = [
+  "saudi", "ksa", "riyadh", "jeddah", "mecca", "medina", "neom",
+  "mohammed bin salman", "mbs", "crown prince",
+  "السعودية", "الرياض", "جدة", "مكة", "المدينة", "نيوم",
+  "محمد بن سلمان", "ولي العهد", "بن سلمان"
+];
